a0b4031a947fb2e86c96002c343395b11b146f33
heyulu <18229317781@163.com>
2026-09-14 16:10:53 +0800

    给npc完善makefile

---
+++ npc/obj_dir/Vtop__Trace__0__Slow.cpp
@@ -0,0 +1,33 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Tracing implementation internals
+
+#include "verilated_fst_c.h"
+#include "Vtop__Syms.h"
+
+
+VL_ATTR_COLD void Vtop___024root__trace_init_top(Vtop___024root* vlSelf, VerilatedFst* tracep) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root__trace_init_top\n"); );
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    auto& vlSelfRef = std::ref(*vlSelf).get();
+    // Body
+    // Empty
+}
+
+VL_ATTR_COLD void Vtop___024root__trace_const_0(void* voidSelf, VerilatedFst::Buffer* bufp);
+void Vtop___024root__trace_cleanup(void* voidSelf, VerilatedFst* /*unused*/);
+
+VL_ATTR_COLD void Vtop___024root__trace_register(Vtop___024root* vlSelf, VerilatedFst* tracep) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root__trace_register\n"); );
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    auto& vlSelfRef = std::ref(*vlSelf).get();
+    // Body
+    tracep->addConstCb(&Vtop___024root__trace_const_0, 0, vlSelf);
+    tracep->addCleanupCb(&Vtop___024root__trace_cleanup, vlSelf);
+}
+
+VL_ATTR_COLD void Vtop___024root__trace_const_0(void* voidSelf, VerilatedFst::Buffer* bufp) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root__trace_const_0\n"); );
+    // Body
+    Vtop___024root* const __restrict vlSelf VL_ATTR_UNUSED = static_cast<Vtop___024root*>(voidSelf);
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+}
---
+++ npc/obj_dir/Vtop___024root__Slow.cpp
@@ -0,0 +1,21 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vtop.h for the primary calling header
+
+#include "Vtop__pch.h"
+
+
+Vtop___024root::Vtop___024root(Vtop__Syms* symsp, const char* namep)
+ {
+    vlSymsp = symsp;
+    vlNamep = strdup(namep);
+    // Reset structure values
+}
+
+void Vtop___024root::__Vconfigure(bool first) {
+    (void)first;  // Prevent unused variable warning
+}
+
+Vtop___024root::~Vtop___024root() {
+    VL_DO_DANGLING(std::free(const_cast<char*>(vlNamep)), vlNamep);
+}
